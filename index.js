2beb8af9e96311429ed9ee8c29a6e6c011362b41
FHieser <47272854+FHieser@users.noreply.github.com>
2022-02-24 16:42:05 +0100

    Updating to correctly send Information

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 const express = require('express');
 const { send } = require('express/lib/response');
+const cors = require('cors')
+
 const Web3 = require('web3');
 const merkletreejs = require('./merkle_tree.js');
 
-
 const app = express()
+app.use(cors())
 const port = 3000
 
 app.get('/', (req, res) => {
 
     try {
-        let address = req.body.address;
-
+        let address = req.query.address;
+        
         if (!Web3.utils.isAddress(address)) {
             res.status(406).send({ message: "Not a recognizable Address" })
+            return
         }
 
-        let { isValid, hexProof } = merkletreejs.getProof(address)
-
-        if (!isValid) {
+        const { isVerified, hexProof } = merkletreejs.getProof(address)
+        
+        if (!isVerified) {
             res.status(406).send({ message: "Not a whitelisted Address" })
+            return
         }
         res.status(200).json({ proof: hexProof }).send()
+        return
     }
     catch (e) {
         res.status(400).send({ message: "Could not process." })
+        return
     }
 
 })
--- merkle_tree.js
+++ merkle_tree.js
@@ -7,21 +7,21 @@ const keccak256 = require('keccak256');
 // 2. Collect list of wallet addresses from competition, raffle, etc.
 // Store list of addresses in some data sheeet (Google Sheets or Excel)
 const whitelistAddresses = [
-    "0X5B38DA6A701C568545DCFCB03FCB875F56BEDDC4",
-    "0X5A641E5FB72A2FD9137312E7694D42996D689D99",
-    "0XDCAB482177A592E424D1C8318A464FC922E8DE40",
-    "0X6E21D37E07A6F7E53C7ACE372CEC63D4AE4B6BD0",
-    "0X09BAAB19FC77C19898140DADD30C4685C597620B",
-    "0XCC4C29997177253376528C05D3DF91CF2D69061A",
-    "0xdD870fA1b7C4700F2BD7f44238821C26f7392148" // The address in remix
-  ];
+  "0X5B38DA6A701C568545DCFCB03FCB875F56BEDDC4",
+  "0X5A641E5FB72A2FD9137312E7694D42996D689D99",
+  "0XDCAB482177A592E424D1C8318A464FC922E8DE40",
+  "0X6E21D37E07A6F7E53C7ACE372CEC63D4AE4B6BD0",
+  "0X09BAAB19FC77C19898140DADD30C4685C597620B",
+  "0XCC4C29997177253376528C05D3DF91CF2D69061A",
+  "0xdD870fA1b7C4700F2BD7f44238821C26f7392148" // The address in remix
+];
 
 // 3. Create a new array of `leafNodes` by hashing all indexes of the `whitelistAddresses`
 // using `keccak256`. Then creates a Merkle Tree object using keccak256 as the algorithm.
 //
 // The leaves, merkleTree, and rootHas are all PRE-DETERMINED prior to whitelist claim
 const leafNodes = whitelistAddresses.map(addr => keccak256(addr));
-const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true});
+const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
 
 // 4. Get root hash of the `merkleeTree` in hexadecimal format (0x)
 // Print out the Entire Merkle Tree.
@@ -29,10 +29,14 @@ const rootHash = merkleTree.getRoot();
 
 // `getHexProof` returns the neighbour leaf and all parent nodes hashes that will
 // be required to derive the Merkle Trees root hash.
-function getProof(claimingAddress){
-  let hexProof= merkleTree.getHexProof(claimingAddress);
-  let isVerified=merkleTree.verify(hexProof, claimingAddress, rootHash);
-  return [isVerified,hexProof];
+function getProof(claimingAddress) {
+  claimingAddress= keccak256(claimingAddress);
+
+  const hexProof = merkleTree.getHexProof(claimingAddress);
+
+  const isVerified = merkleTree.verify(hexProof, claimingAddress, rootHash);
+
+  return {isVerified, hexProof};
 }
 
-exports.getProof=getProof;
+exports.getProof = getProof;
